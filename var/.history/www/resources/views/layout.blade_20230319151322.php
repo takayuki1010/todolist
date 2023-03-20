ea6fabdb109e06f4b996b9b0e02aa8a8f4c5ca0b
takayuki1010 <takayuki.tokusatu8@gmail.com>
2023-03-20 12:40:29 +0900

    トップの画面改装

--- var/.history/www/resources/views/layout.blade_20230319151322.php
+++ var/.history/www/resources/views/layout.blade_20230319151322.php
@@ -17,26 +17,6 @@
     <header>
         <nav class="my-navbar">
             <a class="my-navbar-brand" href="#">ToDo App</a>
-            <div class="my-navbar-control">
-                @if(Auth::check())
-                    <span class="my-navbar-item">
-                        ようこそ、{{ Auth::user()->name }}さん
-                    </span>
-                    ｜
-                    <a href="" id="logout" class="my-navbar-item">ログアウト</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                    </form>
-                    @else
-                        <a href="{{ route('login') }}" class="my-navbar-item">
-                            ログイン
-                        </a>
-                        ｜
-                    <a href="{{ route('register') }}" class="my-navbar-item">
-                        会員登録
-                    </a>
-                @endif
-            </div>
         </nav>
     </header>
 
